53021717cec02d0026a2cb9ad54bc213502e5b05
Dilyana Dimitrova <dilyanadimitrova55@gmail.com>
2024-10-24 14:02:32 +0900

    send email with attachments

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+// Get __filename and __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config()
 
@@ -11,6 +18,7 @@ var transport = nodemailer.createTransport({
       pass: process.env.smtp_password
     }
   });
+
 
 function sendEmail(){
     const mailOptions = {
@@ -39,4 +47,68 @@ function sendEmail(){
     })
 }
 
-sendEmail();
+
+function sendEmailWithAttachment(){
+  const mailOptions = {
+      from: {
+        name: "Dilyana Dimitrova",
+        address: 'dilly@email.bg'
+      },
+      to: 'user@domain.com',
+      subject: 'Email test.',
+      text: 'Email with Attachment.',
+      /*
+      attachments: {
+        filename: 'attachment_1.txt',
+        content: 'Hello, user, this is a text file.'
+      }
+       */
+      attachments: [
+        {
+          filename: 'attachment_1.txt',
+          content: 'Hello, user, this is a text file.'
+        },
+        {
+          filename: 'attachment_2.jpg',
+          path: path.join(__dirname, 'public', 'picture_1.jpg') 
+        }]
+      
+  }
+
+  transport.sendMail(mailOptions, (error, info) => {
+    if(error)
+      return console.log(`Error: ${error}`);
+
+    return console.log(`Email sent successfully. Info: ${JSON.stringify(info)}`);       
+  })
+}
+
+
+function sendEmailWithEmbeddedImage(){
+  const mailOptions = {
+      from: {
+        name: "Dilyana Dimitrova",
+        address: 'dilly@email.bg'
+      },
+      to: 'user@domain.com',
+      subject: 'Email test.',
+      html: 'Email with Embedded Image <br> <img width=500 src="cid:unique_id@domain.com"/>',
+      attachments: 
+        {
+          filename: 'attachment_2.jpg',
+          path: path.join(__dirname, 'public', 'picture_1.jpg'),
+          cid: 'unique_id@domain.com'
+        }      
+  }
+
+  transport.sendMail(mailOptions, (error, info) => {
+    if(error)
+      return console.log(`Error: ${error}`);
+
+    return console.log(`Email sent successfully. Info: ${JSON.stringify(info)}`);       
+  })
+}
+
+// sendEmail();
+// sendEmailWithAttachment();
+sendEmailWithEmbeddedImage();
